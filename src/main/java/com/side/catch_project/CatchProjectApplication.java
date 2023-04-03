b4a8a50df19e2fffc863597102e880e13664ea1e
kkr <kkr0787@naver.com>
2023-04-03 20:52:52 +0900

    Initial commit

---
+++ src/main/java/com/side/catch_project/CatchProjectApplication.java
@@ -0,0 +1,13 @@
+package com.side.catch_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatchProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatchProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/side/catch_project/CatchProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.side.catch_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatchProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
